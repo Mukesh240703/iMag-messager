06e671b7c87e779d682a7d4373c732622146dc4e
Mukesh240703 <apivision@iMagnumhealthcare.com>
2026-01-09 14:48:51 +0530

    fix: lazy load MONGODB_URI to fix docker build

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
-}
 
 /*
  * Global is used here to maintain a cached connection across hot reloads
@@ -26,12 +22,18 @@ if (!cached) {
     cached = global.mongoose = { conn: null, promise: null };
 }
 
+
 async function connectDB() {
     if (cached.conn) {
         return cached.conn;
     }
 
     if (!cached.promise) {
+        const MONGODB_URI = process.env.MONGODB_URI;
+        if (!MONGODB_URI) {
+            throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+        }
+
         const opts = {
             bufferCommands: false,
         };
